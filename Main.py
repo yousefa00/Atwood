a09b0f57776f40b5de848bd431ebf0fda7b2eb4a
Yousef <yousefahmed0066@gmail.com>
2018-11-28 13:57:54 -0600

    Add friction

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-#Original way to create a basic screen
+# Original way to create a basic screen
 # import pygame
 # pygame.init()
 #
@@ -22,8 +22,8 @@
 #
 # pygame.quit()
 
-#The link below shows and explains how to drop spheres with gravity playing a role with Pymunk and Pygames interacting.
-#https://github.com/viblo/pymunk/blob/08fb141b81c0240513fc16e276d5ade5b0506512/docs/html/_sources/tutorials/SlideAndPinJoint.rst.txt
+# The link below shows and explains how to drop spheres with gravity playing a role with Pymunk and Pygames interacting.
+# https://github.com/viblo/pymunk/blob/08fb141b81c0240513fc16e276d5ade5b0506512/docs/html/_sources/tutorials/SlideAndPinJoint.rst.txt
 
 import sys, random
 import pygame
@@ -31,52 +31,104 @@ from pygame.locals import *
 import pymunk
 import pymunk.pygame_util
 
-def add_ball(space):
-    mass = 1
-    radius = 20
-    moment = pymunk.moment_for_circle(mass, 0, radius) #sets moment of inertia
-    body = pymunk.Body(mass, moment) #creates body
-    x = random.randint(120, 380)
-    body.position = x, 550 #sets position, (x, y) coordinate with x generated randomly
-    shape = pymunk.Circle(body, radius) #body needs to be defined as a shape to collide with things
-    space.add(body, shape) #add shape to the space
-    return shape
+import math, sys, random
+
+import pygame
+from pygame.locals import *
+from pygame.color import *
+
+import pymunk
+from pymunk import Vec2d
+import pymunk.pygame_util
+
+
+def draw_collision(arbiter, space, data):
+    for c in arbiter.contact_point_set.points:
+        r = max(3, abs(c.distance * 5))
+        r = int(r)
+
+        p = pymunk.pygame_util.to_pygame(c.point_a, data["surface"])
+        pygame.draw.circle(data["surface"], THECOLORS["black"], p, r, 1)
+
 
 def main():
+    global contact
+    global shape_to_remove
+
     pygame.init()
     screen = pygame.display.set_mode((600, 600))
-    pygame.display.set_caption("Object Dropping, Affect of Gravity")
     clock = pygame.time.Clock()
+    running = True
 
+    ### Physics stuff
     space = pymunk.Space()
-    space.gravity = (0, -980) #x-coordinate is for gravity in x-direction, y-coordinate for gravity in y-direction
-
-
-    balls = []
+    space.gravity = (0.0, -900.0)
     draw_options = pymunk.pygame_util.DrawOptions(screen)
+    # disable the build in debug draw of collision point since we use our own code.
+    draw_options.flags = draw_options.flags ^ pymunk.pygame_util.DrawOptions.DRAW_COLLISION_POINTS
+    ## Balls
+    balls = []
 
+    ### walls
+    static_lines = [pymunk.Segment(space.static_body, (11.0, 280.0), (407.0, 246.0), 0.0)
+        , pymunk.Segment(space.static_body, (407.0, 246.0), (407.0, 343.0), 0.0)
+                    ]
+    for l in static_lines:
+        l.friction = 0.5
+    space.add(static_lines)
 
     ticks_to_next_ball = 10
-    while True:
+
+    ch = space.add_collision_handler(0, 0)
+    ch.data["surface"] = screen
+    ch.post_solve = draw_collision
+
+    while running:
         for event in pygame.event.get():
             if event.type == QUIT:
-                sys.exit(0)
+                running = False
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
-                sys.exit(0)
+                running = False
+            elif event.type == KEYDOWN and event.key == K_p:
+                pygame.image.save(screen, "contact_with_friction.png")
 
         ticks_to_next_ball -= 1
         if ticks_to_next_ball <= 0:
-            ticks_to_next_ball = 25
-            ball_shape = add_ball(space)
-            balls.append(ball_shape)
+            ticks_to_next_ball = 100
+            mass = 0.1
+            radius = 25
+            inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
+            body = pymunk.Body(mass, inertia)
+            x = random.randint(115, 350)
+            body.position = x, 400
+            shape = pymunk.Circle(body, radius, (0, 0))
+            shape.friction = 0.5
+            space.add(body, shape)
+            balls.append(shape)
 
-        space.step(1/50.0) #amount of steps the object takes downwards at a time.
+        ### Clear screen
+        screen.fill(THECOLORS["white"])
 
-        screen.fill((255,255,255))
+        ### Draw stuff
         space.debug_draw(draw_options)
 
+        balls_to_remove = []
+        for ball in balls:
+            if ball.body.position.y < 200: balls_to_remove.append(ball)
+        for ball in balls_to_remove:
+            space.remove(ball, ball.body)
+            balls.remove(ball)
+
+        ### Update physics
+        dt = 1.0 / 60.0
+        for x in range(1):
+            space.step(dt)
+
+        ### Flip screen
         pygame.display.flip()
-        clock.tick(40)
+        clock.tick(50)
+        pygame.display.set_caption("fps: " + str(clock.get_fps()))
+
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
